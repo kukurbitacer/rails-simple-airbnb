d64ab7ecd4b79bb651f418091e1fe21d87c19fde
camille raiser <camille.raiser@hotmail.fr>
2018-08-08 17:34:21 +0200

    styled homepage and show flat logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Nice appartement in Paris Montparnasse',
+  address: '24 avenue Montparnasse, Paris',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 120,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Time Square cute private studio',
+  address: 'Hells Kitchen, New York',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Private studio in Sol',
+  address: 'Madrid',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 46,
+  number_of_guests: 4
+)
